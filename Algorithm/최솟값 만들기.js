a875555f63fd7b789651d171167082d34b17690e
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-09-26 22:26:50 +0900

    최솟값 만들기

---
+++ Algorithm/최솟값 만들기.js
@@ -0,0 +1,10 @@
+function solution(A, B) {
+  let answer = 0;
+
+  let sortA = A.sort((a, b) => a - b);
+  let sortB = B.sort((a, b) => b - a);
+  for (let i = 0; i < sortA.length; i++) {
+    answer += A[i] * B[i];
+  }
+  return answer;
+}
