75140c9b5f9d6439323702d4acafac3f104c1cf6
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-10-10 21:32:27 +0900

    숫자 짝꿍

---
+++ Algorithm/숫자 짝꿍.js
@@ -0,0 +1,22 @@
+function solution(X, Y) {
+  let X = "100";
+  let Y = "2345";
+  let answer = "";
+
+  X = [...X];
+  Y = [...Y];
+
+  for (let i = 0; i < 10; i++) {
+    const X_cnt = X.filter((item) => +item === i).length;
+    const Y_cnt = Y.filter((item) => +item === i).length;
+    answer += i.toString().repeat(Math.min(X_cnt, Y_cnt));
+  }
+
+  if (answer === "") {
+    return "-1";
+  } else if (answer.match(/[^0]/g) === null) {
+    return "0";
+  } else {
+    return [...answer].sort().reverse().join("");
+  }
+}
