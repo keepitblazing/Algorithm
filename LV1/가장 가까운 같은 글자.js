c889f763e14e29398bacc564b7008226cbce6def
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-08-26 17:18:04 +0900

    LV1 : add 가장 가까운 같은 글자.js

---
+++ LV1/가장 가까운 같은 글자.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  const check = {};
+
+  return [...s].map((value, index) => {
+    if (check[value] !== undefined) {
+      const frontDistance = index - check[value];
+      check[value] = index;
+      return frontDistance;
+    } else {
+      check[value] = index;
+      return -1;
+    }
+  });
+}
