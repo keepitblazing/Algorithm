b8d37b6c87132ea696d030c1a948cbc83e0aa975
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-09-16 22:24:00 +0900

    LV1: solve 명예의 전당.js

---
+++ 명예의 전당.js
@@ -0,0 +1,12 @@
+// 명예의 전당
+
+function solution(k, score) {
+  let answer = [];
+  let nums = [];
+  score.forEach((num) => {
+    nums.push(num);
+    nums.sort((a, b) => b - a).splice(k);
+    answer.push(Math.min.apply(null, nums));
+  });
+  return answer;
+}
