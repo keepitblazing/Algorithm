8975d9c2c0d09ce3b7c51ddd6f952ccb10b5ee9b
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2023-11-11 00:21:20 +0900

    LV0: add 특수문자 출력하기.java

--- master.sh
+++ master.sh
@@ -3,7 +3,7 @@
 echo "Select a Level: 0, 1, 2"
 read level
 
-echo "Enter the name of the file you created or modified:"
+echo "Enter the name of the file you created or modified (without extension):"
 read filename
 
 dir_name="LV$level"
@@ -15,16 +15,31 @@ fi
 
 cd $dir_name
 
-exec 3>&1           
-exec > README.md    
+exec 3>&1
+exec > README.md
 
-echo "| 순서 | 문제 |"
-echo "|-------|-----------|"
+if [ "$level" -eq 0 ]; then
+    echo "| 순서 | 문제 | JS | Java |"
+    echo "|-------|-----------|-----|------|"
+else
+    echo "| 순서 | 문제 |"
+    echo "|-------|-----------|"
+fi
 
 index=1
 for file in *; do
-    if [ -f "$file" ] && [ "$file" != "README.md" ]; then  
-        echo "| $index | $file |"
+    if [ -f "$file" ] && [ "$file" != "README.md" ]; then
+        if [ "$level" -eq 0 ]; then
+            js_mark="X"
+            java_mark="X"
+
+            [[ -f "${file}.js" ]] && js_mark="O"
+            [[ -f "${file}.java" ]] && java_mark="O"
+
+            echo "| $index | $file | $js_mark | $java_mark |"
+        else
+            echo "| $index | $file |"
+        fi
         ((index++))
     fi
 done
@@ -39,6 +54,13 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
+file_extension=""
+if [ "$level" -eq 0 ]; then
+    file_extension=".js"
+elif [ "$level" -eq 1 ]; then
+    file_extension=".java"
+fi
+
 git add .
-git commit -m "$dir_name: add $filename"
+git commit -m "$dir_name: add $filename$file_extension"
 git push
