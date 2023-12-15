00e4f0596685ef32061fb483dc6372bd48c47944
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2023-12-16 00:11:34 +0900

    LV1: add 수박수박수박수박수.java

--- LV1/가운데 글자 가져오기.js
+++ LV1/가운데 글자 가져오기.js
@@ -1,11 +1,5 @@
 function solution(s) {
-  let answer = "";
-
-  if (s.length % 2 == 1) {
-    answer = s[(s.length - 1) / 2];
-  } else {
-    answer = s[s.length / 2 - 1] + s[s.length / 2];
-  }
-
-  return answer;
+  return s.length % 2 == 1
+    ? s[(s.length - 1) / 2]
+    : s[s.length / 2 - 1] + s[s.length / 2];
 }
---
+++ LV1/수박수박수박수박수?.go
@@ -0,0 +1,11 @@
+func solution(n int) string {
+	var answer string
+	for i := 0; i < n; i++ {
+		if i % 2 == 0 {
+			answer += "수"
+		} else {
+			answer += "박"
+		}
+	}
+    return answer
+}
