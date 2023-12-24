f22cb568fb91c1abab93215cb419abfa50f98d45
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2023-12-24 14:22:26 +0900

    LV1: add 같은 숫자는 싫어.java

---
+++ LV1/같은 숫자는 싫어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        
+        ArrayList<Integer> tempList = new ArrayList<>();
+
+        tempList.add(arr[0]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                tempList.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[tempList.size()];
+        for (int i = 0; i < tempList.size(); i++) {
+            answer[i] = tempList.get(i);
+        }
+
+        return answer;
+    }
+}
