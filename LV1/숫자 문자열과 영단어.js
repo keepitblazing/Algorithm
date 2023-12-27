e36c4fe7b429ee4fd454aa3314d090b35c5d3410
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2023-12-27 11:35:03 +0900

    LV1: refactor 숫자 문자열과 영단어.js

--- LV1/숫자 문자열과 영단어.js
+++ LV1/숫자 문자열과 영단어.js
@@ -1,17 +1,20 @@
 function solution(s) {
-  let answer = 0;
+  const numberWords = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
 
-  s = s.replace(/zero/g, 0);
-  s = s.replace(/one/g, 1);
-  s = s.replace(/two/g, 2);
-  s = s.replace(/three/g, 3);
-  s = s.replace(/four/g, 4);
-  s = s.replace(/five/g, 5);
-  s = s.replace(/six/g, 6);
-  s = s.replace(/seven/g, 7);
-  s = s.replace(/eight/g, 8);
-  s = s.replace(/nine/g, 9);
+  for (const [word, number] of Object.entries(numberWords)) {
+    s = s.replace(new RegExp(word, "g"), number);
+  }
 
-  answer = Number(s);
-  return answer;
+  return Number(s);
 }
