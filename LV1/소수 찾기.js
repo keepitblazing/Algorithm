751f7ac646813bcd97d2bcf116af53f6136ece71
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-01-01 17:57:24 +0900

    LV1: add 소수찾기.py

--- LV1/소수 찾기.js
+++ LV1/소수 찾기.js
@@ -1,16 +1,16 @@
 function solution(n) {
-  let answer = 0;
-
-  for (let i = 2; i <= n; i++) {
-    if (checkPrime(i)) answer++;
+  let sieve = new Array(n + 1).fill(true);
+  let m = Math.floor(Math.sqrt(n));
+  for (let i = 2; i <= m; i++) {
+    if (sieve[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        sieve[j] = false;
+      }
+    }
   }
-  return answer;
-}
-function checkPrime(isPrimeNumber) {
-  if (isPrimeNumber === 2) return true; //2는 소수이기 때문에 true 리턴
-  if (isPrimeNumber % 2 === 0) return false; // 2를 제외한 짝수는 소수 x false 리턴
-  for (let i = 3; i * i <= isPrimeNumber; i += 2) {
-    if (isPrimeNumber % i === 0) return false;
-  } //for문을 돌려서 홀수일 경우 소수 판단
-  return true;
+  let count = 0;
+  for (let i = 2; i <= n; i++) {
+    if (sieve[i]) count++;
+  }
+  return count;
 }
