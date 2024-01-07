cae0debf4af706b168671df9119b1430d8430a33
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-01-07 20:01:01 +0900

    LV0: refactor 첫 번째로 나오는 음수.js

--- LV0/첫 번째로 나오는 음수.js
+++ LV0/첫 번째로 나오는 음수.js
@@ -1,12 +1,8 @@
 function solution(num_list) {
-  let answer = 0;
   for (let i = 0; i < num_list.length; i++) {
     if (num_list[i] < 0) {
-      answer = i;
-      break;
-    } else {
-      answer = -1;
+      return i;
     }
   }
-  return answer;
+  return -1;
 }
