533f0d2ce8d26ec0cde46e66debd0d85043d0b18
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-01-09 08:30:11 +0900

    LV0: add n보다 커질 때까지 더하기.java

---
+++ LV0/n보다 커질 때까지 더하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public int solution(int[] numbers, int n) {
+    return Arrays
+      .stream(numbers)
+      .reduce(0, (acc, i) -> n >= acc ? acc + i : acc);
+  }
+}
