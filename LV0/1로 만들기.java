b4caca58d18a5882c23a4c1beb067cda02d649f7
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-01-08 12:28:05 +0900

    LV1: add 글자 지우기.java

---
+++ LV0/1로 만들기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public int solution(int[] num_list) {
+    return Arrays
+      .stream(num_list)
+      .map(i -> Integer.toBinaryString(i).length() - 1)
+      .sum();
+  }
+}
