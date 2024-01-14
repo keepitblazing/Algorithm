232fa2206b88e8daf51293ca53b849d1b1d24865
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-01-14 12:50:25 +0900

    LV0: add 배열 조각하기.js

--- LV0/문자열 돌리기.java
+++ LV0/문자열 돌리기.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Solution {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String a = sc.next();
+    Scanner newScanner = new Scanner(System.in);
+    String a = newScanner.next();
     for (char ch : a.toCharArray()) System.out.println(ch);
   }
 }
--- LV0/배열 조각하기.js
+++ LV0/배열 조각하기.js
@@ -1,11 +1,18 @@
 function solution(arr, query) {
-  let result = arr;
-  for (let i = 0; i < query.length; i++) {
-    if (i === 0 || i % 2 === 0) {
-      result = result.slice(0, query[i] + 1);
-    } else {
-      result = result.slice(query[i]);
-    }
-  }
-  return result;
+  return query.reduce(
+    (acc, cur, i) => (i % 2 === 0 ? acc.slice(0, cur + 1) : acc.slice(cur)),
+    arr
+  );
 }
+
+// function solution(arr, query) {
+//   let result = arr;
+//   for (let i = 0; i < query.length; i++) {
+//     if (i === 0 || i % 2 === 0) {
+//       result = result.slice(0, query[i] + 1);
+//     } else {
+//       result = result.slice(query[i]);
+//     }
+//   }
+//   return result;
+// }
