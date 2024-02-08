8031b9a1eccd3250489a2a0d9b5f0b790cc03ab7
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-02-08 18:55:13 +0900

    LV0: add 홀짝에 따라 다른 값 반환하기.java

---
+++ LV0/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,19 @@
+class Solution {
+  public void solution(n: int) {
+    let answer = 0;
+    if (n % 2 !== 0) {
+      for (let i = 0; i < n + 1; i++) {
+        if (i % 2 !== 0 || i === 1) {
+          answer += i;
+        }
+      }
+    } else {
+      for (let i = 0; i < n + 1; i++) {
+        if (i % 2 === 0) {
+          answer += i * i;
+        }
+      }
+    }
+    return answer;
+  }
+}
