ee0169b503bfaf2b2fb494bdec9917436eef0e89
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-02-24 07:59:16 +0900

    LV1: add 올바른 괄호.java

---
+++ LV2/올바른 괄호.java
@@ -0,0 +1,17 @@
+class Solution {
+
+  public boolean solution(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == '(') {
+        count++;
+      } else {
+        count--;
+      }
+      if (count < 0) {
+        return false;
+      }
+    }
+    return count == 0;
+  }
+}
