5de4a7e17c2b5957a91bcd5d6a0d93c33c7adb2f
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-03-23 19:08:10 +0900

    LV2: refactor 다리를 지나는 트럭.js

--- LV2/다리를 지나는 트럭.js
+++ LV2/다리를 지나는 트럭.js
@@ -1,22 +1,20 @@
-function solution(bridge_length, weight, truck_weights) {
+function solution(bridge_length, max_weight, truck_weights) {
   let time = 0;
-  let bridge = Array.from({ length: bridge_length }, () => 0);
-  let bridgeWeight = 0;
+  let bridge = [];
+  let totalWeightOnBridge = 0;
 
-  time++;
-  bridge.shift();
-  bridgeWeight += truck_weights[0];
-  bridge.push(truck_weights.shift());
-
-  while (bridgeWeight > 0) {
+  while (bridge.length > 0 || truck_weights.length > 0) {
     time++;
-    bridgeWeight -= bridge.shift();
-
-    if (truck_weights.length > 0 && bridgeWeight + truck_weights[0] <= weight) {
-      bridgeWeight += truck_weights[0];
-      bridge.push(truck_weights.shift());
-    } else {
-      bridge.push(0);
+    if (bridge.length > 0 && bridge[0].time === time) {
+      totalWeightOnBridge -= bridge.shift().weight;
+    }
+    if (
+      truck_weights.length &&
+      totalWeightOnBridge + truck_weights[0] <= max_weight
+    ) {
+      let weight = truck_weights.shift();
+      totalWeightOnBridge += weight;
+      bridge.push({ weight: weight, time: time + bridge_length });
     }
   }
 
