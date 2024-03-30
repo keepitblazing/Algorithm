67b81cc8cf37e2ba9cbdc63ba2a596884efd039d
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-03-30 15:42:34 +0900

    LV2: recap 타겟 넘버.js

--- LV2/타겟 넘버.js
+++ LV2/타겟 넘버.js
@@ -1,16 +1,16 @@
 function solution(numbers, target) {
-    let answer = 0;
-    const length = numbers.length
-    getAnswer(0,0);
-    function getAnswer(x, v) {
-        if(x < length){
-            getAnswer(x + 1, v + numbers[x]);
-            getAnswer(x + 1, v - numbers[x]);
-        } else{
-            if(v === target){
-                answer++
-            }
-        }
+  let answer = 0;
+  const length = numbers.length;
+  getAnswer(0, 0);
+  function getAnswer(x, v) {
+    if (x < length) {
+      getAnswer(x + 1, v + numbers[x]);
+      getAnswer(x + 1, v - numbers[x]);
+    } else {
+      if (v === target) {
+        answer++;
+      }
     }
-    return answer;
+  }
+  return answer;
 }
