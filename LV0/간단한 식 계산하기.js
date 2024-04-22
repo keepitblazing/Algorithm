17a0b10acce62726d946e88f229eb2e6cf23fbc5
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-04-22 19:42:36 +0900

    LV0: refactor 간단한 식 계산하기.js

--- LV0/간단한 식 계산하기.js
+++ LV0/간단한 식 계산하기.js
@@ -1,3 +1,16 @@
 function solution(binomial) {
-  return eval(binomial);
+  binomial = binomial.split(" ");
+  let first = parseInt(binomial[0]);
+  let second = binomial[1];
+  let third = parseInt(binomial[2]);
+  switch (second) {
+    case "+":
+      return first + third;
+    case "-":
+      return first - third;
+    case "*":
+      return first * third;
+    case "/":
+      return first / third;
+  }
 }
