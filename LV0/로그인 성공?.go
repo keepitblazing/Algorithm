aff302bb891536afc9c7207d854ac7c8536c87ca
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-04-24 22:53:00 +0900

    LV0: add 로그인 성공?.go

---
+++ LV0/로그인 성공?.go
@@ -0,0 +1,12 @@
+func solution(id_pw, db) {
+    for (let i = 0; i < db.length; i++) {
+        if (id_pw[0] === db[i][0]) {
+            if (id_pw[1] === db[i][1]) {
+                return "login";
+            } else {
+                return "wrong pw";
+            }
+        }
+    }
+    return "fail";
+}
