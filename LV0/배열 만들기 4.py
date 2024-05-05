22fd67750423fed28bfdf0ef6ce21050c0c8a233
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-05-05 22:43:03 +0900

    LV0: add 배열 만들기 4.py

---
+++ LV0/배열 만들기 4.py
@@ -0,0 +1,13 @@
+def solution(arr){
+  answer = []
+  for i in range(len(arr)):
+    if len(answer) == 0:
+      answer.append(arr[i])
+    elif answer[-1] < arr[i]:
+      answer.append(arr[i])
+    else:
+      while len(answer) > 0 and answer[-1] >= arr[i]:
+        answer.pop()
+      answer.append(arr[i])
+  return answer
+}
