31a4ebf24108d583289c50c5c779285e0d970094
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-05-27 22:15:30 +0900

    LV0: add 배열 만들기 1.java

---
+++ LV0/배열 만들기 1.java
@@ -0,0 +1,12 @@
+class Solution {
+
+  public int[] solution(int n, int k) {
+    int[] answer = {};
+    for (int i = 1; i <= n; i++) {
+      if (i % k == 0) {
+        answer[answer.length] = i;
+      }
+    }
+    return answer;
+  }
+}
