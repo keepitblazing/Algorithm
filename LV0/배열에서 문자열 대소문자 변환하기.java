2171015db37b1c491947afb4410066235e380644
JIMIN PARK <103014298+keepitblazing@users.noreply.github.com>
2024-06-15 16:00:56 +0900

    LV0: add 배열에서 문자열 대소문자 변환하기.java

---
+++ LV0/배열에서 문자열 대소문자 변환하기.java
@@ -0,0 +1,12 @@
+class Solution {
+
+  public String[] solution(String[] strArr) {
+    for (int i = 0; i < strArr.length; i++) {
+      strArr[i] =
+        i == 0 || i % 2 == 0
+          ? strArr[i].toLowerCase()
+          : strArr[i].toUpperCase();
+    }
+    return strArr;
+  }
+}
